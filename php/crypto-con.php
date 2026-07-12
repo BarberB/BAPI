b005f03054d96c984306424f994efce8ad4ae682
Ben Barber <ben@barberpolefilms.com>
2026-07-12 11:08:16 -0700

    Suppress CLI deprecation noise

--- php/crypto-con.php
+++ php/crypto-con.php
@@ -1,5 +1,11 @@
 <?php
 
+if (PHP_SAPI === 'cli') {
+    ini_set('display_errors', '0');
+    ini_set('log_errors', '1');
+    error_reporting(E_ALL & ~E_DEPRECATED & ~E_USER_DEPRECATED);
+}
+
 $dbConfig = require '/etc/web-applications/trading-app/database.php';
 
 $mysqli = new mysqli(
